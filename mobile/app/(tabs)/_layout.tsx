8146dcf6d030721b22d80977153586af1b301e6a
nthyznh13 <nathalya.bezerra.santos@escola.pr.gov.br>
2025-10-27 12:23:03 -0300

    colocando icones

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -28,6 +29,13 @@ export default function TabLayout() {
         options={{
           title: 'Procurar',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="alan"
+        options={{
+          title: 'Bookstore',
+          tabBarIcon: ({ color }) => <Ionicons name="cart-outline" size={24} color="white" />,
         }}
       />
     </Tabs>    
