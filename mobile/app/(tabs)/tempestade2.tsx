6ef27402a39a7225b817ebbfd842935b0c51e2a0
gabrielsource6 <gabriel.galvao.campos@escola.pr.gov.br>
2025-10-29 12:26:08 -0300

    alteração de paginas

---
+++ mobile/app/(tabs)/tempestade2.tsx
@@ -0,0 +1,111 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+import { Collapsible } from '@/components/ui/collapsible';
+import { ExternalLink } from '@/components/external-link';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Fonts } from '@/constants/theme';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={
+         <Image
+         source={require('@/assets/images/logo-livraria.png')}
+         style={styles.image}
+         />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText
+          type="title"
+          style={{
+            fontFamily: Fonts.rounded,
+          }}>
+          data de entrega 
+        </ThemedText>
+      </ThemedView>
+      <ThemedText>inicio da compra</ThemedText>
+      <Collapsible title=" compra ✅">
+        <ThemedText>
+          você comprou 1 livro
+          <ThemedText type="defaultSemiBold"> livro: tempestade de onix</ThemedText>
+          <ThemedText type="defaultSemiBold"> por   R$ 59,64.</ThemedText>
+        </ThemedText>
+        <ThemedText>
+           <ThemedText type="defaultSemiBold"></ThemedText>
+        </ThemedText>
+        <ExternalLink href="https://docs.expo.dev/router/introduction">
+          <ThemedText type="link"></ThemedText>
+        </ExternalLink>
+      </Collapsible>
+      <Collapsible title="endereço de sua residencia e entrega✅ ">
+        <ThemedText>
+          Rua ourizona 1350, data de entrega: 15/10
+          <ThemedText type="defaultSemiBold"></ThemedText> 
+        </ThemedText>
+      </Collapsible>
+      <Collapsible title="o livro✅ ">
+        <ThemedText>
+           <ThemedText type="defaultSemiBold"></ThemedText>  
+          <ThemedText type="defaultSemiBold"></ThemedText>
+        </ThemedText>
+        <Image
+          source={require('@/assets/images/tempestade.png')}
+          style={{ width: 100, height: 100, alignSelf: 'center' }}
+        />
+        <ExternalLink href="https://reactnative.dev/docs/images">
+          <ThemedText type="link"></ThemedText>
+        </ExternalLink>
+      </Collapsible>
+      <Collapsible title="descrição do livro✅">
+        <ThemedText>
+        Sinopse "Tempestade de Ônix" é a sequência da série "Empíreo",
+        onde a protagonista Violet Sorrengail enfrenta novos desafios após sobreviver ao primeiro ano na Academia de Guerra. Neste livro,
+        Violet deve lidar com perigos ainda maiores, tanto físicos quanto políticos,
+        enquanto busca aliados em terras desconhecidas para enfrentar inimigos poderosos que ameaçam seu mundo.
+        A narrativa é rica em ação, com sequências emocionantes de voos com dragões e batalhas intensas,
+        mantendo o ritmo acelerado que cativou os leitores no primeiro volume.        
+          <ThemedText type="defaultSemiBold"></ThemedText>
+        </ThemedText>
+      </Collapsible>
+      <Collapsible title="agradecimentos✅">
+        <ThemedText>
+          <ThemedText type="defaultSemiBold">um agradecimento ao nosso cliente : gabriel ricardo</ThemedText> 
+          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
+          </ThemedText>
+          Muito obrigado por comprar na nossa loja.
+        </ThemedText>
+        {Platform.select({
+          ios: (
+            <ThemedText>
+             <ThemedText type="defaultSemiBold">seu porduto esta entregue</ThemedText>
+            </ThemedText>
+          )
+        })}
+      </Collapsible>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+   image: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    alignSelf: 'center', 
+    position: 'absolute',
+  }
+});
---
+++ mobile/app/(tabs)/verity1.tsx
@@ -0,0 +1,112 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+import { Collapsible } from '@/components/ui/collapsible';
+import { ExternalLink } from '@/components/external-link';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Fonts } from '@/constants/theme';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={
+         <Image
+         source={require('@/assets/images/logo-livraria.png')}
+         style={styles.image}
+         />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText
+          type="title"
+          style={{
+            fontFamily: Fonts.rounded,
+          }}>
+          data de entrega 
+        </ThemedText>
+      </ThemedView>
+      <ThemedText>inicio da compra</ThemedText>
+      <Collapsible title=" compra ✅">
+        <ThemedText>
+          você comprou 1 livro
+          <ThemedText type="defaultSemiBold"> livro: verity</ThemedText>
+          <ThemedText type="defaultSemiBold"> por   R$ 68,90.</ThemedText>
+        </ThemedText>
+        <ThemedText>
+           <ThemedText type="defaultSemiBold"></ThemedText>
+          
+        </ThemedText>
+        <ExternalLink href="https://docs.expo.dev/router/introduction">
+          <ThemedText type="link"></ThemedText>
+        </ExternalLink>
+      </Collapsible>
+      <Collapsible title="endereço de sua residencia e entrega✅ ">
+        <ThemedText>
+          Rua ourizona 1350, data de entrega: 15/10
+          <ThemedText type="defaultSemiBold"></ThemedText> 
+        </ThemedText>
+      </Collapsible>
+      <Collapsible title="o livro✅ ">
+        <ThemedText>
+           <ThemedText type="defaultSemiBold"></ThemedText>  
+          <ThemedText type="defaultSemiBold"></ThemedText>
+        </ThemedText>
+        <Image
+          source={require('@/assets/images/verity.png')}
+          style={{ width: 100, height: 100, alignSelf: 'center' }}
+        />
+        <ExternalLink href="https://reactnative.dev/docs/images">
+          <ThemedText type="link"></ThemedText>
+        </ExternalLink>
+      </Collapsible>
+      <Collapsible title="descrição do livro✅">
+        <ThemedText>
+        "Verity", de Colleen Hoover, é um suspense psicológico sobre Lowen Ashleigh,
+         uma escritora contratada para terminar a série de livros da autora Verity Crawford após um acidente.
+          Ao vasculhar os manuscritos de Verity,
+           Lowen encontra um diário que revela segredos perturbadores e chocantes sobre a verdadeira natureza da autora,
+            sua família e o que pode ter acontecido com sua filha. A história combina elementos de suspense, romance e mistério, questionando a
+             verdade por trás das revelações e o que pode estar escondido na mente das pessoas. "
+          <ThemedText type="defaultSemiBold"></ThemedText>
+        </ThemedText>
+      </Collapsible>
+      <Collapsible title="agradecimentos✅">
+        <ThemedText>
+          <ThemedText type="defaultSemiBold">um agradecimento ao nosso cliente : gabriel ricardo</ThemedText> 
+          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
+          </ThemedText>
+          Muito obrigado por comprar na nossa loja.
+        </ThemedText>
+        {Platform.select({
+          ios: (
+            <ThemedText>
+             <ThemedText type="defaultSemiBold">seu porduto esta entregue</ThemedText>
+            </ThemedText>
+          )
+        })}
+      </Collapsible>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+   image: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    alignSelf: 'center', 
+    position: 'absolute',
+  }
+});
